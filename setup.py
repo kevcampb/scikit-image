d3f483da57e303a47ced41185e30385de4064a60
Kevin Campbell <kev@projectcolo.org.uk>
2016-04-27 12:18:48 +0800

    Allow the use of the egg_info command supplied to setup.py This fixes issues when scikit-image is listed in a requirements.txt file but numpy is not installed. With this change, pip will download numpy before attempting to install this package.

--- setup.py
+++ setup.py
@@ -88,7 +88,8 @@ if __name__ == "__main__":
         if len(sys.argv) >= 2 and ('--help' in sys.argv[1:] or
                                    sys.argv[1] in ('--help-commands',
                                                    '--version',
-                                                   'clean')):
+                                                   'clean',
+                                                   'egg_info')):
             # For these actions, NumPy is not required.
             #
             # They are required to succeed without Numpy for example when
